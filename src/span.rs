b284450007e1d3095871c042cce0e44486280ece
Kylee Tilley <kyleetilley@gmail.com>
2026-06-04 23:35:50 -0500

    Add comments and test to span

--- src/span.rs
+++ src/span.rs
@@ -2,6 +2,7 @@ use std::ops::Range;
 
 use bon::bon;
 
+/// A range and position of text within source text
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Span {
     pub start: u32,
@@ -22,7 +23,24 @@ impl Span {
         }
     }
 
-    pub fn slice<'a>(&self, src: &'a str) -> &'a str {
-        &src[self.start as usize..self.end as usize]
+    /// Get the slice of text the span represents in the source text
+    pub fn slice<'a>(&self, source: &'a str) -> &'a str {
+        &source[self.start as usize..self.end as usize]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn slice() {
+        let source = "Hello\nWorld";
+
+        let span = Span::builder().range(0..4).line(1).column(5).build();
+        assert_eq!("Hell", span.slice(source));
+
+        let span = Span::builder().range(6..11).line(2).column(6).build();
+        assert_eq!("World", span.slice(source));
     }
 }
