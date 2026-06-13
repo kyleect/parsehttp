d49bac929bd5ac695cfd3bdc2d3f3c090226a168
kyleect <kyleetilley@gmail.com>
2026-06-13 01:31:24 +0000

    Deploying to gh-pages from @ kyleect/parsehttp@6c4b61520366ea8b34477898d568dcbeb3aa9797 🚀

--- api/trait.impl/core/default/trait.Default.js
+++ api/trait.impl/core/default/trait.Default.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["parsehttp",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"parsehttp/struct.HttpVersion.html\" title=\"struct parsehttp::HttpVersion\">HttpVersion</a>",0]]]]);
+    const implementors = Object.fromEntries([["parsehttp",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"parsehttp/struct.HttpRequestSpans.html\" title=\"struct parsehttp::HttpRequestSpans\">HttpRequestSpans</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"parsehttp/struct.HttpResponseSpans.html\" title=\"struct parsehttp::HttpResponseSpans\">HttpResponseSpans</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"parsehttp/struct.HttpVersion.html\" title=\"struct parsehttp::HttpVersion\">HttpVersion</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[293]}
+//{"start":59,"fragment_lengths":[882]}
